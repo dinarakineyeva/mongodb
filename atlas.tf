f74fb86ad77718f1f217e30e459204d6a414153a
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-04-27 11:44:12 -0400

    Update atlas.tf

--- atlas.tf
+++ atlas.tf
@@ -1,92 +1,20 @@
-
-
-provider "mongodbatlas" {
-  public_key  = var.atlas_pub_key
-  private_key = var.atlas_priv_key
-}
-
-resource "mongodbatlas_project" "demo" {
-  name   = local.project_id
-  org_id = var.atlas_org_id
-}
-
-resource "mongodbatlas_project_ip_access_list" "acl" {
-  project_id = mongodbatlas_project.demo.id
-  cidr_block = "0.0.0.0/0"
-}
-
 resource "mongodbatlas_cluster" "cluster" {
-  project_id = mongodbatlas_project.demo.id
-  name       = local.project_id
-  cluster_type           = "REPLICASET"
-  provider_name               = "GCP"
-  # backing_provider_name       = "GCP"
-  provider_region_name        = var.atlas_cluster_region
-  provider_instance_size_name = var.atlas_cluster_tier
-  cloud_backup                 = true
-  auto_scaling_disk_gb_enabled            = true
-  mongo_db_major_version                  = "4.4"
-  # auto_scaling_compute_enabled            = true
-  # auto_scaling_compute_scale_down_enabled = true
-
-  labels {
-    key   = "environment"
-    value = "prod"
-  }
-
+  project_id             = var.project_id_mongo
+  name                   = var.cluster_name
+  mongo_db_major_version = var.mongodbversion
+  cluster_type           = var.cluster_type
   replication_specs {
-    num_shards = 1
+    num_shards = var.num_shards
     regions_config {
-      region_name     = "CENTRAL_US"
-      electable_nodes = 3
-      priority        = 7
-      read_only_nodes = 0
+      region_name     = var.region_name
+      electable_nodes = var.electable_nodes
+      priority        = var.priority
+      read_only_nodes = var.read_only_nodes
     }
   }
+  # Provider Settings "block"
+  cloud_backup                 = var.cloud_backup
+  auto_scaling_disk_gb_enabled = var.auto_scaling_disk_gb_enabled
+  provider_name                = var.cloud_provider
+  provider_instance_size_name  = var.provider_instance_size_name
 }
-
-resource "mongodbatlas_database_user" "user" {
-  project_id         = mongodbatlas_project.demo.id
-  auth_database_name = "admin"
-
-  username = var.db_user
-  password = random_string.mongodb_password.result
-
-  roles {
-    role_name     = "readWrite"
-    database_name = var.db_name
-  }
-}
-
-locals {
-  # the demo app only takes URIs with the credentials embedded and the atlas
-  # provider doesn't give us a good way to get the hostname without the protocol
-  # part so we end up doing some slicing and dicing to get the creds into the URI
-  atlas_uri = replace(
-    mongodbatlas_cluster.cluster.srv_address,
-    "://",
-    "://${var.db_user}:${mongodbatlas_database_user.user.password}@"
-  )
-}
-
-# resource "mongodbatlas_cloud_backup_schedule" "test" {
-#   project_id   = mongodbatlas_cluster.cluster.project_id
-#   cluster_name = mongodbatlas_cluster.cluster.name
-
-#   reference_hour_of_day    = 3
-#   reference_minute_of_hour = 45
-#   restore_window_days      = 4
-
-
-#   // This will now add the desired policy items to the existing mongodbatlas_cloud_backup_schedule resource
-#   policy_item_hourly {
-#     frequency_interval = 1
-#     retention_unit     = "days"
-#     retention_value    = 1
-#   }
-#   policy_item_daily {
-#     frequency_interval = 1
-#     retention_unit     = "days"
-#     retention_value    = 2
-#   }
-# }
