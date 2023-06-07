2242f7973289501bf0bd407f2f737e5e54eed6fd
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-06-07 11:15:25 -0400

    Update privatelink.tf

--- privatelink.tf
+++ privatelink.tf
@@ -2,32 +2,48 @@ resource "mongodbatlas_privatelink_endpoint" "mongoatlas_primary" {
   project_id    = var.project_id_mongo
   provider_name = "GCP"
   region        = var.gcp_region
+  # depends_on = [ mongodbatlas_cluster.cluster ]
+}
+
+# ===== Create a Google Network ===== #
+resource "google_compute_network" "default" {
+  project = var.gcp_project
+  name    = var.network_name
+}
+
+# ==== Create a Google Sub Network ===== #
+resource "google_compute_subnetwork" "default" {
+  project       = google_compute_network.default.project
+  name          = var.subnet_name 
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.gcp_region
+  network       = google_compute_network.default.id
 }
 
 
-# Create Google 10 Addresses
+# ===== Create Google 50 Addresses ===== #
 resource "google_compute_address" "compute_address" {
   count        = 50
-  project      = var.gcp_project
-  name         = "test-mongo-${count.index}"
-  subnetwork   = var.subnet_name
+  project      = google_compute_subnetwork.default.project
+  name         = "${var.compute_address_name}-${count.index}"
+  subnetwork   = google_compute_subnetwork.default.id
   address_type = var.google_compute_address_type
-  # address      = "${var.google_compute_address}${count.index + 5}"
-  address      = "10.128.0.${count.index + 3}"
-  region = var.gcp_region
+  address      = "${var.google_compute_address}${count.index}"
+  # address = "10.0.42.${count.index}"
+  region  = google_compute_subnetwork.default.region
 
   depends_on = [mongodbatlas_privatelink_endpoint.mongoatlas_primary]
 }
 
-# Create 10 Forwarding rules
+# ==== Create 50 Forwarding rules ===== #
 resource "google_compute_forwarding_rule" "compute_forwarding_rule" {
   count                 = 50
-  project               = var.gcp_project
-  region                = var.gcp_region
-  name                  = "test-mongo${count.index}"
+  project               = google_compute_address.compute_address[count.index].project
+  region                = google_compute_address.compute_address[count.index].region
+  name                  = google_compute_address.compute_address[count.index].name
   target                = mongodbatlas_privatelink_endpoint.mongoatlas_primary.service_attachment_names[count.index]
   ip_address            = google_compute_address.compute_address[count.index].id
-  network               = var.network_name
+  network               = google_compute_network.default.id
   load_balancing_scheme = ""
 }
 
@@ -35,7 +51,7 @@ resource "mongodbatlas_privatelink_endpoint_service" "test" {
   project_id          = mongodbatlas_privatelink_endpoint.mongoatlas_primary.project_id
   private_link_id     = mongodbatlas_privatelink_endpoint.mongoatlas_primary.private_link_id
   provider_name       = "GCP"
-  endpoint_service_id = var.network_name
+  endpoint_service_id = google_compute_network.default.name
   gcp_project_id      = var.gcp_project
 
   dynamic "endpoints" {
@@ -43,10 +59,9 @@ resource "mongodbatlas_privatelink_endpoint_service" "test" {
 
     content {
       ip_address    = google_compute_address.compute_address[endpoints.key].address
-      endpoint_name = google_compute_address.compute_address[endpoints.key].name
+      endpoint_name = google_compute_forwarding_rule.compute_forwarding_rule[endpoints.key].name
     }
   }
 
   depends_on = [google_compute_forwarding_rule.compute_forwarding_rule]
 }
-
