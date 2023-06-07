6c0f96c880c0b17ca5e5bf9e96b70d41a591ea01
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-06-07 11:15:04 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
 output "user1" {
   value = mongodbatlas_database_user.user.username
 }
+
+output "database-rw-user" {
+  value = mongodbatlas_database_user.database-rw-user.username
+}
+
+output "database-admin-user" {
+  value = mongodbatlas_database_user.database-admin-user.username
+}
+
+
+output "connection_string" {
+  value = length(local.connection_strings) > 0 ? local.connection_strings[0] : ""
+}
