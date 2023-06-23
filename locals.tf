c1c26435a5cd5ef8c2d015cf9145ce3f17f73fed
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-06-23 01:38:24 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,10 +1,19 @@
 locals {
+  region_str    = split("-", var.gcp_region)
+  region_letter = substr(local.region_str[1], 0, 1)
+  region_number = substr(local.region_str[1], length(local.region_str[1]) - 1, 1)
+  cluster_name  = lower("mongodb${local.region_letter}${local.region_number}${var.environment}")
+
   mongodb_database_name = "db"
   mongodb_rw_role_actions = [
     "FIND", "INSERT", "REMOVE", "UPDATE", "BYPASS_DOCUMENT_VALIDATION", "CREATE_COLLECTION", "CREATE_INDEX",
     "DROP_COLLECTION", "CHANGE_STREAM", "COLL_MOD", "COMPACT", "CONVERT_TO_CAPPED", "DROP_INDEX", "RE_INDEX",
     "COLL_STATS", "DB_HASH", "LIST_INDEXES", "VALIDATE"
   ]
+  mongodb_ro_role_actions = [
+    "FIND", "BYPASS_DOCUMENT_VALIDATION", "COLL_MOD", "COLL_STATS", "DB_HASH", "LIST_INDEXES", "VALIDATE"
+  ]
+
   mongodb_admin_role_actions = [
     "ENABLE_PROFILER", "DROP_DATABASE", "RENAME_COLLECTION_SAME_DB", "DB_STATS", "LIST_COLLECTIONS"
   ]
@@ -12,10 +21,12 @@ locals {
     "read", "readWrite", "dbAdmin"
   ]
 
-  mongodb_rw_username    = "${local.mongodb_database_name}_rw"
-  mongodb_rw_password    = random_password.database-rw-password.result
-  mongodb_admin_username = "${local.mongodb_database_name}_admin"
-  mongodb_admin_password = random_password.database-admin-password.result
+  mongodb_ro_username    = "${var.environment}_${local.mongodb_database_name}_ro"
+  mongodb_ro_password    = random_password.database-password.result
+  mongodb_rw_username    = "${var.environment}_${local.mongodb_database_name}_rw"
+  mongodb_rw_password    = random_password.database-password.result
+  mongodb_admin_username = "${var.environment}_${local.mongodb_database_name}_admin"
+  mongodb_admin_password = random_password.database-password.result
 
 
   endpoint_service_id = google_compute_network.default.name
